9197654d571ac6ed7f1d94111a5f0c7977d0370a
peterrrockii <53229013+peterrrockii@users.noreply.github.com>
2023-02-12 12:06:45 -0700

    added mouse and asdw handling

--- Remembering_OpenGL/src/main.cpp
+++ Remembering_OpenGL/src/main.cpp
@@ -30,6 +30,31 @@
 #include "tests/TestCube.h"
 
 
+// camera
+glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
+glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
+glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
+
+
+// timing
+float deltaTime = 0.0f;	// time between current frame and last frame
+float lastFrame = 0.0f;
+
+
+bool firstMouse = true;
+float yaw = -90.0f;	// yaw is initialized to -90.0 degrees since a yaw of 0.0 results in a direction vector pointing to the right so we initially rotate a bit to the left.
+float pitch = 0.0f;
+float lastX = 960.0f / 2.0;
+float lastY = 540.0f / 2.0;
+float fov = 45.0f;
+
+
+// Move these into abstracted places later
+void processInput(GLFWwindow* window);
+void mouse_callback(GLFWwindow* window, double xposIn, double yposIn);
+
+void registerTests(test::TestMenu* menu, int wWidth, int wHeight);
+
 
 int main(void)
 {
@@ -59,6 +84,10 @@ int main(void)
 
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
+    glfwSetCursorPosCallback(window, mouse_callback);
+
+
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);//capture the mouse
 
 
     // only draw as fast as vsync
@@ -75,24 +104,15 @@ int main(void)
     std::cout << glGetString(GL_VERSION) << std::endl;
 
     {
-
-        GLfloat mat_specular[] = { 1.0, 1.0, 1.0, 1.0 };
-        GLfloat mat_shininess[] = { 50.0 };
-        GLfloat light_position[] = { 1.0, 1.0, 1.0, 0.0 };
         glClearColor(0.0, 0.0, 0.0, 0.0);
         glShadeModel(GL_SMOOTH);
 
-        glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
-        glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess);
-        glLightfv(GL_LIGHT0, GL_POSITION, light_position);
-        glEnable(GL_LIGHTING);
-        glEnable(GL_LIGHT0);
-        GLCall(glEnable(GL_BLEND));
+
         GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
         GLCall(glEnable(GL_DEPTH_TEST));
 
         Renderer renderer;
-
+        
         ImGui::CreateContext();
         ImGui_ImplGlfwGL3_Init(window, true);
         ImGui::StyleColorsDark();
@@ -103,17 +123,18 @@ int main(void)
         test::TestMenu* testMenu = new test::TestMenu(currentTest);
         currentTest = testMenu;
 
-        testMenu->RegisterTest<test::TestClearColor>("Clear Color");
-        testMenu->RegisterTest<test::TestTexture2D>("2D Texture", wWidth, wHeight);
-        testMenu->RegisterTest<test::TestTriangle>("Triangle");
-        testMenu->RegisterTest<test::TestCube>("Cube", wWidth, wHeight);
+        registerTests(testMenu, wWidth, wHeight);
+
 
         test::TestCube cube(wWidth, wHeight);
 
-    
 
         while (!glfwWindowShouldClose(window))
         {
+            float currentFrame = static_cast<float>(glfwGetTime());
+            deltaTime = currentFrame - lastFrame;
+            lastFrame = currentFrame;
+
             GLCall(glClearColor(0.0f, 0.0f, 0.0f, 1.0f));
             /* Render here */
             renderer.Clear();
@@ -121,18 +142,18 @@ int main(void)
             // resize the viewport to the window size
             glfwGetWindowSize(window, &wWidth, &wHeight);
             GLCall(glViewport(0, 0, wWidth, wHeight));
-          
+
+            processInput(window);
+      
 
             glm::mat4 proj = glm::perspective(glm::radians(45.0f), (float)wWidth / (float)wHeight, 0.1f, 100.0f);
-            glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -3.0f));
+            glm::mat4 view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
 
             cube.OnRender(proj, view);
 
 
 
             glFlush();
-
-            glfwGetWindowSize(window, &wWidth, &wHeight);
 
             ImGui_ImplGlfwGL3_NewFrame();
             if (currentTest)
@@ -149,7 +170,6 @@ int main(void)
                 ImGui::End();
             }
 
-
             ImGui::Render();
             ImGui_ImplGlfwGL3_RenderDrawData(ImGui::GetDrawData());
 
@@ -164,4 +184,87 @@ int main(void)
     ImGui::DestroyContext();
     glfwTerminate();
     return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void processInput(GLFWwindow* window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+
+    float cameraSpeed = static_cast<float>(2.5 * deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+        cameraPos += cameraSpeed * cameraFront;
+
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        cameraPos -= cameraSpeed * cameraFront;
+
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+        cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+        cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+}
+
+
+void mouse_callback(GLFWwindow* window, double xposIn, double yposIn)
+{
+    float xpos = static_cast<float>(xposIn);
+    float ypos = static_cast<float>(yposIn);
+
+    if (firstMouse)
+    {
+        lastX = xpos;
+        lastY = ypos;
+        firstMouse = false;
+    }
+
+    float xoffset = xpos - lastX;
+    float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
+    lastX = xpos;
+    lastY = ypos;
+
+    float sensitivity = 0.1f; // change this value to your liking
+    xoffset *= sensitivity;
+    yoffset *= sensitivity;
+
+    yaw += xoffset;
+    pitch += yoffset;
+
+    // make sure that when pitch is out of bounds, screen doesn't get flipped
+    if (pitch > 89.0f)
+        pitch = 89.0f;
+    if (pitch < -89.0f)
+        pitch = -89.0f;
+
+    glm::vec3 front;
+    front.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+    front.y = sin(glm::radians(pitch));
+    front.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+    cameraFront = glm::normalize(front);
+}
+
+
+void registerTests(test::TestMenu* menu, int wWidth, int wHeight)
+{
+    menu->RegisterTest<test::TestClearColor>("Clear Color");
+    menu->RegisterTest<test::TestTexture2D>("2D Texture", wWidth, wHeight);
+    menu->RegisterTest<test::TestTriangle>("Triangle");
+    menu->RegisterTest<test::TestCube>("Cube", wWidth, wHeight);
 }
